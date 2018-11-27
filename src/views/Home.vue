c7b6814b52517aad73bae87400ab5941e6ff1db5
dereksmith83 <mdsmith1983@gmail.com>
2018-11-26 21:38:06 -0600

    popups in an hash

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    <div id='map'></div>
+    <div id="map"></div>
     <!-- blog app -->
 <!--     <div class="container">
       
@@ -26,11 +26,14 @@
 </template>
 
 <style>
-  body { margin:0; padding:0; }
-  #map { 
-    height: 400px;
-    width: 100%;
-  }
+body {
+  margin: 0;
+  padding: 0;
+}
+#map {
+  height: 400px;
+  width: 100%;
+}
 </style>
 
 <script>
@@ -39,27 +42,54 @@
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      message: "Welcome to Vue.js!",
+      places: [
+        { lat: 41.947460, lng: -87.654730, description: "Home of Elwood Blues" },
+        { lat:41.852180, lng: -87.808490, description: "Largest Dead Clown Graveyard in World" }
+      ]
     };
   },
   created: function() {},
   mounted: function() {
-    mapboxgl.accessToken = 'pk.eyJ1IjoicGV0ZXJ4amFuZyIsImEiOiJjam94YWZieXExYnQ0M3BucXZwbmV2Y2VsIn0.pmLbX0YPw86c5r8YlyZr7w';
+    mapboxgl.accessToken =
+      "pk.eyJ1IjoicGV0ZXJ4amFuZyIsImEiOiJjam94YWZieXExYnQ0M3BucXZwbmV2Y2VsIn0.pmLbX0YPw86c5r8YlyZr7w";
     var map = new mapboxgl.Map({
-      container: 'map', // container id
-      style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
-      center: [-74.50, 40], // starting position [lng, lat]
-      zoom: 9 // starting zoom
+    //   container: 'map', // container id
+    //   style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
+    //   center: [-87.640010, 41.779830], // starting position [lng, lat]
+    //   zoom: 9 // starting zoom
+    container: "map", // container id
+      style: "mapbox://styles/mapbox/streets-v9", // stylesheet location
+      center: [this.places[0].lng, this.places[0].lat], // starting position [lng, lat]
+      zoom: 3 // starting zoom
     });
-    var marker = new mapboxgl.Marker()
-    .setLngLat([-74.50, 40])
-    .addTo(map);
+     // for (var i = 0; i < this.places.length; i++) {
+    //   var place = this.places[i];
+    //   var popup = new mapboxgl.Popup({ offset: 25 }).setText(place.description);
+    //   var marker = new mapboxgl.Marker()
+    //     .setLngLat([place.lng, place.lat])
+    //     .setPopup(popup)
+    //     .addTo(map);
+    // }
+    this.places.forEach(function(place) {
+      var popup = new mapboxgl.Popup({ offset: 25 }).setText(place.description);
+      var marker = new mapboxgl.Marker()
+        .setLngLat([place.lng, place.lat])
+        .setPopup(popup)
+        .addTo(map);
+    });
+    // var popup = new mapboxgl.Popup({ offset: 25 })
+    //   .setText("H.H. Holmes MURDER CASTLE was located at 63rd and Wallace Street. Now a Post Office");
+    // var marker = new mapboxgl.Marker()
+    // .setLngLat([-87.640010, 41.779830])
+    // .setPopup(popup)
+    // .addTo(map);
   },
   methods: {},
   computed: {}
 };
 
-// Blog App
+// Blog app
 // var axios = require("axios");
 
 // export default {
